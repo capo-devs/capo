30bf560e6527382e730eddb746885adfcfc6c6cd
alexdevteam <alexpro820@gmail.com>
2021-09-30 18:09:04 +0200

    Add new formats to example

--- example/example.cpp
+++ example/example.cpp
@@ -1,17 +1,35 @@
 #include <capo/capo.hpp>
+#include <capo/types.hpp>
 #include <ktl/str_format.hpp>
 #include <chrono>
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <thread>
+#include <tuple>
+#include <vector>
 
 namespace impl {
 static constexpr int fail_code = 2;
 // TODO: Add parameters for each one of these constants [Use clap?]
 static constexpr float travel_circurference_radius = 2.0f;
 static constexpr float travel_angular_speed = 2.0f;
-static constexpr bool loop_audio = true;
+static constexpr bool loop_audio = false;
+/* clang-format off */
+static std::vector<std::tuple<std::string, capo::FileFormat>> supportedFormats{
+	{".wav", capo::FileFormat::eWav},
+	{".flac", capo::FileFormat::eFlac},
+	{".mp3", capo::FileFormat::eMp3}
+};
+/* clang-format on */
+
+std::optional<capo::FileFormat> formatFromFilename(std::string name) {
+	for (auto const& [extension, format] : supportedFormats) {
+		if (name.ends_with(extension)) { return format; }
+	}
+	return std::nullopt;
+}
 
 // Uniform circular motion formulas
 capo::Vec3 UCMPosition(float angularSpeed, float time, float radius) {
@@ -29,13 +47,23 @@ int openAlTest(std::string const& wavPath, float gain, bool loop) {
 		return impl::fail_code;
 	}
 
-	auto pcm = capo::PCM::fromFile(wavPath);
+	capo::FileFormat format;
+	if (auto extension = formatFromFilename(wavPath)) {
+		format = extension.value();
+	} else {
+		std::cerr << "File format not supported. Supported formats: ";
+		for (auto const& [extension, _] : supportedFormats) { std::cerr << extension << " "; }
+		std::cerr << std::endl;
+		return fail_code;
+	}
+
+	auto pcm = capo::PCM::fromFile(wavPath, format);
 	if (!pcm) {
-		std::cerr << "Couldn't load audio file." << std::endl;
+		std::cerr << "Couldn't load audio file. (Error: " << static_cast<int>(pcm.error()) << ")" << std::endl;
 		return impl::fail_code;
 	}
 	capo::Sound sound = instance.makeSound(*pcm);
-	
+
 	capo::Source source = instance.makeSource();
 	source.gain(gain);
 	if (!source.bind(sound)) {
@@ -45,9 +73,16 @@ int openAlTest(std::string const& wavPath, float gain, bool loop) {
 	source.loop(loop);
 	source.play();
 
-	std::cout << ktl::format("Playing {} once at {.2f} gain, length: {.1f}s\n", wavPath, gain, sound.length().count());
-	std::cout << ktl::format("Travelling on a circurference around the listener; r={.1f}, angular speed={.1f}\n", travel_circurference_radius,
-							 travel_angular_speed);
+	// FIXME ktl::format does not display values correctly (open issue in repo with minimal example)
+	std::cout << ktl::format("{} info:\n\t{.2f}s Length\n\t{} Channels\n\t{}Hz Sample Rate\n\t{} PCM Size\n", wavPath, sound.length().count(), pcm->meta.channels,
+							 pcm->meta.rate , pcm->size);
+	std::cout << ktl::format("Playing {} once at {.2f} gain\n", wavPath, gain);
+	if (pcm->meta.channels == 1) {
+		std::cout << ktl::format("Travelling on a circurference around the listener; r={.1f}, angular speed={.1f}\n", travel_circurference_radius,
+								 travel_angular_speed);
+	} else {
+		std::cout << "Warning: Input has more than one channel, positional audio is disabled" << std::endl;
+	}
 
 	int done{};
 	auto start = std::chrono::high_resolution_clock::now();
